2970869a9a787a7934dc8bda45c79dba5e80f64f
Megh-Thakkar <megh.1211@gmail.com>
2018-05-27 04:26:55 +0530

    Changed serializer format acc to Frontend req

--- main/serializers.py
+++ main/serializers.py
@@ -2,16 +2,31 @@ from rest_framework import serializers
 from .models import *
 
 class DownloadSerializer(serializers.ModelSerializer):
+    property = serializers.SerializerMethodField()
+
+    def get_property(self, component):
+        return component.downloads
+
     class Meta:
         model = Component
-        fields = ('name', 'downloads', 'id', 'url_name')
+        fields = ('name', 'property', 'id', 'url_name')
 
 class StarSerializer(serializers.ModelSerializer):
+    property = serializers.SerializerMethodField()
+
+    def get_property(self, component):
+        return component.stars
+
     class Meta:
         model = Component
-        fields = ('name', 'stars', 'id', 'url_name')
+        fields = ('name', 'property', 'id', 'url_name')
 
 class RecentComponentSerializer(serializers.ModelSerializer):
+    property = serializers.SerializerMethodField()
+
+    def get_property(self, component):
+        return component.modified_time
+
     class Meta:
         model = Component
-        fields = ('name', 'modified_time', 'id', 'url_name')
+        fields = ('name', 'property', 'id', 'url_name')
