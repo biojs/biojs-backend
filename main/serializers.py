758af5f36fa79a5b73b6bb14896706ec42b60ed9
tusharGoel1 <megh.1211@gmail.com>
2018-06-01 01:52:25 +0530

    Added API response for component details

--- main/serializers.py
+++ main/serializers.py
@@ -30,3 +30,84 @@ class RecentComponentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Component
         fields = ('name', 'property', 'id', 'url_name')
+
+class TopComponentSerializer(serializers.ModelSerializer):  # Data fields when viewing the overall top components
+    tags = serializers.SerializerMethodField()
+    icon_url = serializers.SerializerMethodField()
+    def get_tags(self, obj):
+        tags = []
+        for t in obj.tags.all():
+            tags.append(t.name)
+        return tags
+    def get_icon_url(self, obj):
+        if obj.icon:
+            request = self.context.get('request')
+            icon_url =  obj.icon.url
+            return request.build_absolute_uri(icon_url)
+        else:
+            return '#'
+    class Meta:
+        model = Component
+        fields = ('name', 'tags', 'icon_url', 'downloads', 'stars', 'modified_time', 'short_description', 'id', 'url_name', 'author')
+
+class DetailComponentSerializer(serializers.ModelSerializer):
+    tags = serializers.SerializerMethodField()
+    icon_url = serializers.SerializerMethodField()
+    def get_tags(self, obj):
+        tags = []
+        for t in obj.tags.all():
+            tags.append(t.name)
+        return tags
+    def get_icon_url(self, obj):
+        if obj.icon:
+            request = self.context.get('request')
+            icon_url =  obj.icon.url
+            return request.build_absolute_uri(icon_url)
+        else:
+            return '#'
+    class Meta:
+        model = Component
+        fields = (
+                'name',
+                'stars',
+                'downloads',
+                'modified_time',
+                'tags',
+                'icon_url',
+                'github_url',
+                'short_description',
+                'url_name',
+                'commits',
+                'forks',
+                'watchers',
+                'no_of_contributors',
+                'version',
+                'author',
+                'author_email',
+                'npm_url',
+                'homepage_url',
+                'license',
+            )
+
+class BaseComponentSerializer(serializers.ModelSerializer): #serializer fields used while querying
+    tags = serializers.SerializerMethodField()
+    def get_tags(self, obj):
+        tags = []
+        for t in obj.tags.all():
+            tags.append(t.name)
+        return tags
+    class Meta:
+        model = Component
+        fields = ('name', 'tags', 'id', 'url_name')
+
+class ContributorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contributor
+        fields = ('username', 'avatar_url')
+
+class ContributionSerializer(serializers.ModelSerializer):
+    contributor = ContributorSerializer(required=True, write_only=True)
+
+    class Meta:
+        model = Contribution
+        fields = ('contributor', 'contributions')
--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,7 @@ app_name = 'datatest'
 urlpatterns = [
 		url(r'^$', views.index, name="index"),
 		url(r'^updatedata/$', views.update_data, name="update_data"),
+		url(r'^all/$', views.all_components, name="all_components"),
+        url(r'^top/$', views.top_components, name="top_components"),
+        url(r'^details/(?P<url_name>\w+)/$', views.component_details, name="component_details"),
 ]
